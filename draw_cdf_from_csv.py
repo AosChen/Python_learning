9b984d29517749250445f284aaa3941e88dc03c6
AosChen <AosChen@hotmail.com>
2018-07-30 14:37:00 +0800

    增加绘制CDF的脚本

--- draw_cdf_from_csv.py
+++ draw_cdf_from_csv.py
@@ -1,39 +1,53 @@
 #  本py文件的作用是绘制cdf图，其输入要求为csv文件，csv文件用','隔开，且每一行代表一条cdf曲线
 import matplotlib.pyplot as plt
 
-filename = r'D:\研究所\惯导安卓\data\0710\20180709195642_HUAWEI NXT-AL10_水平放置_副驾驶_error.csv'  # CSV文件路径
+Title = ['20-30km/h Straight cdf', '40-60km/h Straight cdf', 'Spiral cdf']
+# filename = r'D:\研究所\惯导安卓\data\0710\直角 my.csv'  # CSV文件路径
+filenames = [r'D:\研究所\惯导安卓\data\0725\20-30.csv', r'D:\研究所\惯导安卓\data\0725\40-60.csv', r'D:\研究所\惯导安卓\data\0725\盘旋.csv']
+    # , r'D:\研究所\惯导安卓\data\0710\直角.csv', r'D:\研究所\惯导安卓\data\0725\盘旋.csv']
+file_lines = []
 
-lines = []
-with open(filename, 'r') as f:
-    lines = f.read().split('\n')
+for filename in filenames:
+    with open(filename, 'r') as f:
+        lines = f.read().split('\n')
+        file_lines.append(lines)
 
 dataSets = []
+for lines in file_lines:
+    dataSet = []
+    for line in lines:
+        # print(line)
+        try:
+            dataSet.append(line.split(','))
+        except:
+            print("Error: Exception Happened... \nPlease Check Your Data Format... ")
+    dataSets.append(dataSet)
 
-for line in lines:
-    # print(line)
-    try:
-        dataSets.append(line.split(','))
-    except:
-        print("Error: Exception Happened... \nPlease Check Your Data Format... ")
+temps = []
+for dataSet in dataSets:
+    temp = []
+    for set in dataSet:
+        temp2 = []
+        for item in set:
+            if item != '':
+                temp2.append(float(item))
+        temp2.sort()
+        temp.append(temp2)
+    temps.append(temp)
+dataSets = temps
 
-temp = []
-for set in dataSets:
-    temp2 = []
-    for item in set:
-        if item != '':
-            temp2.append(float(item))
-    temp2.sort()
-    temp.append(temp2)
-dataSets = temp
-
-for set in dataSets:
-
-    plotDataset = [[], []]
-    count = len(set)
-    for i in range(count):
-        plotDataset[0].append(float(set[i]))
-        plotDataset[1].append((i + 1) / count)
-    print(plotDataset)
-    plt.plot(plotDataset[0], plotDataset[1], '-', linewidth=2)
-
+j = 0
+for dataSet in dataSets:
+    plt.subplot(220+j+1)
+    plt.title(Title[j])
+    plt.grid(True)
+    for set in dataSet:
+        plotDataset = [[], []]
+        count = len(set)
+        for i in range(count):
+            plotDataset[0].append(float(set[i]))
+            plotDataset[1].append((i + 1) / count)
+        plt.plot(plotDataset[0], plotDataset[1], '-', linewidth=2)
+    j += 1
+# plt.grid(color='b', linewidth='0.3', linestyle='--')
 plt.show()
